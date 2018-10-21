bfa4d9996a8ec9b97429ae43acf9a087345a0120
jose <joseluismc81@gmail.com>
2018-10-21 03:23:58 +0200

    [REFACTOR] Use of scanner reading by lines and refactorized command Exe method

--- src/control/Controller.java
+++ src/control/Controller.java
@@ -4,6 +4,7 @@ import logic.Game;
 import logic.Level;
 
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Controller {
     private Game game;
@@ -44,40 +45,74 @@ public class Controller {
 
     private void comandExe() {
         System.out.print("Command > ");
-        String comando;
-        comando = in.next();
-        comando = comando.toLowerCase(); //pone el string entero a minusculas
-        if (comando.equals("add") || comando.equals("a")) {
-            comando = in.next();
-            comando.toLowerCase();
-            int i, j;
-            i = in.nextInt();
-            j = in.nextInt();
-            if (comando.equals("p")) {
-                game.addPeashooter(i, j);
-            } else if (comando.equals("s")) {
-                game.addSunflower(i, j);
-            }
-        } else if (comando.equals("list") || comando.equals("l")) {
-            System.out.println("[S]unflower: Cost: 20 suncoins Harm: 0");
-            System.out.println("[P]eashooter: Cost: 50 suncoins Harm: 1");
+        String inputLine = in.nextLine().toLowerCase();
 
-        } else if (comando.equals("reset") || comando.equals("r")) {
-            game = new Game(level);
-            game.setLevel(level);
-        } else if (comando.equals("help") || comando.equals("h")) {
-            System.out.println("Add <plant> <x> <y>: Adds a plant in position x, y.");
-            System.out.println("List: Prints the list of available plants.");
-            System.out.println("Reset: Starts a new game.");
-            System.out.println("Help: Prints this help message.");
-            System.out.println("Exit: Terminates the program.");
-            System.out.println("[none]: Skips cycle.");
-        } else if (comando.equals("exit") || comando.equals("e")) {
-            System.out.println("Game Over");
-            finish = true;
-        } else if (comando.equals("none") || comando.equals("n") || comando.equals(null)) {
-
+        if (inputLine.isEmpty()) {
+            return;
         }
 
+        StringTokenizer tokenizer = new StringTokenizer(inputLine, " ");
+
+        String command = tokenizer.nextToken();
+
+        switch (command) {
+            case "add":
+            case "a":
+                addPlant(tokenizer);
+                break;
+            case "list":
+            case "l":
+                list();
+                break;
+            case "reset":
+            case "r":
+                reset();
+                break;
+            case "help":
+            case "h":
+                help();
+                break;
+            case "exit":
+            case "e":
+                exit();
+                break;
+
+        }
+    }
+
+    private void exit() {
+        System.out.println("Game Over");
+        finish = true;
+    }
+
+    private void help() {
+        System.out.println("Add <plant> <x> <y>: Adds a plant in position x, y.");
+        System.out.println("List: Prints the list of available plants.");
+        System.out.println("Reset: Starts a new game.");
+        System.out.println("Help: Prints this help message.");
+        System.out.println("Exit: Terminates the program.");
+        System.out.println("[none]: Skips cycle.");
+    }
+
+    private void reset() {
+        game = new Game(level);
+        game.setLevel(level);
+    }
+
+    private void list() {
+        System.out.println("[S]unflower: Cost: 20 suncoins Harm: 0");
+        System.out.println("[P]eashooter: Cost: 50 suncoins Harm: 1");
+    }
+
+    private void addPlant(StringTokenizer tokenizer) {
+        String plant = tokenizer.nextToken();
+        int i = Integer.valueOf(tokenizer.nextToken());
+        int j = Integer.valueOf(tokenizer.nextToken());
+
+        if (plant.equals("p")) {
+            game.addPeashooter(i, j);
+        } else if (plant.equals("s")) {
+            game.addSunflower(i, j);
+        }
     }
 }
